6f6e47ef31bc0b0ed19e760965a4cbbb26d776d4
larsfinlay <larsfinlay@googlemail.com>
2019-07-24 09:47:18 +0100

    Init README.md

---
+++ CarsAPI/Controllers/CarsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarsAPI.Controllers
+{
+    public class CarsController
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class ValuesController : ControllerBase
+        {
+            // GET api/values
+            [HttpGet]
+            public ActionResult<IEnumerable<string>> Get()
+            {
+                return new string[] { "car1", "car2" };
+            }
+
+            // GET api/values/5
+            [HttpGet("{id}")]
+            public ActionResult<string> Get(int id)
+            {
+                return "car";
+            }
+
+            // POST api/values
+            [HttpPost]
+            public void Post([FromBody] string value)
+            {
+            }
+
+            // PUT api/values/5
+            [HttpPut("{id}")]
+            public void Put(int id, [FromBody] string value)
+            {
+            }
+
+            // DELETE api/values/5
+            [HttpDelete("{id}")]
+            public void Delete(int id)
+            {
+            }
+        }
+    }
+}
