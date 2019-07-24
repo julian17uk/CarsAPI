1974f0233ffb1debf80745aca88fa42e284b24c1
larsfinlay <larsfinlay@googlemail.com>
2019-07-24 16:43:44 +0100

    CarDeleteShould.CallRepoDeleteCar :: Red

--- CarsAPITest/ServiceTests/CarServiceShould.cs
+++ CarsAPITest/ServiceTests/CarServiceShould.cs
@@ -13,37 +13,37 @@ namespace CarsAPITest.ServiceTests
         static CarService carService;
         static Mock<ICarRepository> mockedCarRepo;
 
-        public CarServiceShould()
-        {
-            mockedCarRepo = new Mock<ICarRepository>();
-            carService = new CarService(mockedCarRepo.Object);
-        }
-
         public class CarCreateShould : CarServiceShould
         {
+            public CarCreateShould()
+            {
+                mockedCarRepo = new Mock<ICarRepository>();
+                carService = new CarService(mockedCarRepo.Object);
+            }
+
             // Car creation data
-            static string make = "new-car";
-            static string model = "mx-nModel";
-            static string colour = "pink";
-            static int year = 2019;
+            private static string _make = "new-car";
+            private static string _model = "mx-nModel";
+            private static string _colour = "pink";
+            private static int _year = 2019;
 
             // Car creation template
             static Car carCreationData = new Car()
             {
-                Make = make,
-                Model = model,
-                Colour = colour,
-                Year = year
+                Make = _make,
+                Model = _model,
+                Colour = _colour,
+                Year = _year
             };
 
             // Expected created car
             static Car expectedCarCreated = new Car()
             {
                 Id = 1,
-                Make = make,
-                Model = model,
-                Colour = colour,
-                Year = year
+                Make = _make,
+                Model = _model,
+                Colour = _colour,
+                Year = _year
             };
 
             [Fact]
@@ -62,6 +62,25 @@ namespace CarsAPITest.ServiceTests
                 Car createdCar = carService.CreateCar(carCreationData);
 
                 createdCar.ShouldBe(expectedCarCreated);
+            }
+        }
+
+        public class CarDeleteShould : CarServiceShould
+        {
+            CarDeleteShould()
+            {
+                mockedCarRepo = new Mock<ICarRepository>();
+                carService = new CarService(mockedCarRepo.Object);
+            }
+
+            [Fact]
+            public void CallRepoDeleteCar()
+            {
+                int id = 5;
+
+                carService.DeleteCar(id);
+
+                mockedCarRepo.Verify(m => m.DeleteCar(id));
             }
         }
     }
