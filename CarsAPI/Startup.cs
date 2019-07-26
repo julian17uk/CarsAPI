f2761aa544cf0ba51478a5cc7aec4f551b9e0ef8
larsfinlay <larsfinlay@googlemail.com>
2019-07-26 16:26:38 +0100

    Added Swagger

--- CarsAPI/Startup.cs
+++ CarsAPI/Startup.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CarsAPI.Repositories;
+﻿using CarsAPI.Repositories;
 using CarsAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CarsAPI
 {
@@ -35,6 +29,11 @@ namespace CarsAPI
 
             services.AddScoped<ICarRepository, SQLCarRepository>();
             services.AddScoped<ICarService, CarService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +50,14 @@ namespace CarsAPI
             }
 
             app.UseHttpsRedirection();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseMvc();
-		}
+        }
     }
 }
